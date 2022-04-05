23793138fb3a888d271b7c8a2e9956d568620cbe
NguyenHoangNam1909 <69390546+NguyenHoangNam1909@users.noreply.github.com>
2022-04-05 23:32:00 +0700

    commit book and bookService

---
+++ LibraryManagement/src/main/java/com/ou/pojo/Book.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ou.pojo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Lightning
+ */
+public class Book {
+    private String name;
+    private String description;
+    private int amount;
+    private int publishingYear;
+    private int publishingCompanyId;
+    private int categoryId;
+    private Date dateOfEntering;
+    private String position;
+
+    public Book() {
+    }
+    
+}
