ebd4be1fd93e2fc50d2a9d3405ea7463b332fcde
BMLinh <100250983+BMLinh@users.noreply.github.com>
2022-04-23 21:37:29 +0700

    Revert "Merge pull request #20 from BMLinh/main"

---
+++ LibraryManagement/src/main/java/com/ou/pojo/User.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ou.pojo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String fullname;
+    private int gender;
+    private Date birth;
+    private String address;
+    private String phone;
+    private int roleId;
+    private int departmentId;
+    private Date createdDate;
+
+    public User() {}
+    
+    public User(int id, String username, String password, String fullname, int gender, Date birth, String address, String phone, int role_id, int department_id, Date createdDate) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.birth = birth;
+        this.address = address;
+        this.phone = phone;
+        this.roleId = role_id;
+        this.departmentId = department_id;
+        this.createdDate = createdDate;
+    }
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the fullname
+     */
+    public String getFullname() {
+        return fullname;
+    }
+
+    /**
+     * @param fullname the fullname to set
+     */
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    /**
+     * @return the gender
+     */
+    public int getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the birth
+     */
+    public Date getBirth() {
+        return birth;
+    }
+
+    /**
+     * @param birth the birth to set
+     */
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the roleId
+     */
+    public int getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * @param roleId the roleId to set
+     */
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    /**
+     * @return the departmentId
+     */
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    /**
+     * @param departmentId the departmentId to set
+     */
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    /**
+     * @return the createdDate
+     */
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    /**
+     * @param createdDate the createdDate to set
+     */
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+}
