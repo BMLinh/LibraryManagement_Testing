e8ad605a46fbca05be820d44566a9c25dc6c7b9b
hoangnam1909 <69390546+NguyenHoangNam1909@users.noreply.github.com>
2022-04-23 21:52:15 +0700

    push lai em yeu

---
+++ LibraryManagement/src/main/java/com/ou/librarymanagement/LoginSuccessfulController.java
@@ -0,0 +1,50 @@
+package com.ou.librarymanagement;
+
+import com.ou.pojo.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginSuccessfulController {
+
+    private LoginController loginController = new LoginController();
+
+    @FXML
+    private Label lbUserId;
+
+    @FXML
+    private Label lbUsername;
+
+    @FXML
+    private Button btnLogout;
+
+    public void displayData(User user) {
+        lbUserId.setText(String.valueOf(user.getId()));
+        lbUsername.setText(user.getUsername());
+    }
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+        logoutExecute();
+    }
+
+    private void logoutExecute() throws IOException {
+        loginController.setUser(null);
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("FXMLLogin.fxml"));
+        Parent root = loader.load();
+        Scene mainScene = new Scene(root);
+        Stage primaryStage = (Stage) btnLogout.getScene().getWindow();
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+}
