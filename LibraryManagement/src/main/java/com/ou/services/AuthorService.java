03b6e023c43b9e7b6b48695a15194d21bd597205
hoangnam1909 <69390546+NguyenHoangNam1909@users.noreply.github.com>
2022-04-26 15:16:47 +0700

    fix lung tung

--- LibraryManagement/src/main/java/com/ou/services/AuthorService.java
+++ LibraryManagement/src/main/java/com/ou/services/AuthorService.java
@@ -16,7 +16,7 @@ public class AuthorService {
             PreparedStatement stm = conn.prepareStatement("SELECT * FROM author WHERE name like concat('%', ?, '%')");
             if (kw == null)
                 kw = "";
-            stm.setString(1, kw.trim());
+            stm.setString(1, kw);
             ResultSet rs = stm.executeQuery();
 
             List<Author> authors = new ArrayList<>();
--- LibraryManagement/src/main/java/com/ou/utils/Utils.java
+++ LibraryManagement/src/main/java/com/ou/utils/Utils.java
@@ -7,6 +7,9 @@ package com.ou.utils;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,35 +18,71 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /**
- *
  * @author Admin
  */
 public class Utils {
-    public static Alert setAlert(String message, Alert.AlertType alertType){
+    public static Alert setAlert(String message, Alert.AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setContentText(message);
         return alert;
     }
-    
+
     public static final java.sql.Date convertUtilToSql(java.util.Date uDate) {
         java.sql.Date sDate = new java.sql.Date(uDate.getTime());
         return sDate;
     }
-    
-    public static final String convertDateToString(Date date){
+
+    public static final String convertDateToString(Date date) {
         SimpleDateFormat F = new SimpleDateFormat("dd/MM/yyyy");
-        
+
         return F.format(date);
     }
 
-    public static final String convertDateTimeToString(Date date){
+    public static final String convertDateTimeToString(Date date) {
         SimpleDateFormat F = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return F.format(date);
     }
 
-    public static final String convertDateToString(Date date, String pattern){
+    public static final String convertDateToString(Date date, String pattern) {
         SimpleDateFormat F = new SimpleDateFormat(pattern);
         return F.format(date);
+    }
+
+    public static final boolean isContainNumber(String str) {
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static final boolean isContainSpecialCharacter(String str) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static String capitalizeWord(String str) {
+        String words[] = str.split(" ");
+        String capitalizeWord = "";
+        for (String w : words) {
+            String first = w.substring(0, 1);
+            String afterfirst = w.substring(1);
+            capitalizeWord += first.toUpperCase() + afterfirst + " ";
+        }
+        return capitalizeWord.trim();
+    }
+
+    public static final String stringNormalization(String str) {
+        String result = str;
+
+        result = result.trim();
+        result = result.replaceAll("\\s+", " ");
+        result = capitalizeWord(result);
+
+        return result;
     }
 
 }
